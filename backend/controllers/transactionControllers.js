de50beb97e9ed27d4474994d6827d0d26dba1d55
Ahmed-Khan-15 <ahmedkhan8967@gmail.com>
2026-07-10 22:46:31 +0500

    Added GetTransaction Function

--- backend/controllers/transactionControllers.js
+++ backend/controllers/transactionControllers.js
@@ -1,50 +1,115 @@
-const getTransactions = (req, res) => {
-    res.json([{
-        id: 1,
-        type: "income",
-        category: "salary",
-        amount: 100
-    },
-    {
-        id: 2,
-        type: "income",
-        category: "commision",
-        amount: 25
-    }]);
+const pool = require("../config/db");
+
+const getTransactions = async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM transactions");
+        res.json(result.rows);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "something went wrong"
+        });
+    }
 };
 
-const createTransaction = (req, res) => {
-    const { type, category, amount, description } = req.body;
+const getTransactionById = async (req, res) => {
+    const { id } = req.params;
 
-    res.status(201).json({
+    const transactionId = Number(id);
 
-        "message": "Transaction created successfully!",
-        "transaction": {
-            type,
-            category,
-            amount,
-            description
-        }
-    });
-};
-
-const getTransactionById = (req,res)=>{
-    const { id } = Number(req.params);
-
-    if(Number.isNaN(id)){
+    if (Number.isNaN(transactionId)) {
         return res.status(400).json({
             message: "Invalid transaction ID"
         });
     }
 
-    res.json({
-        id,
-        type: "expense",
-        categor: "Food",
-        amount: 500
-    });
+    try {
+        const result = await pool.query("SELECT * FROM transactions WHERE id = $1;", [transactionId]);
+        if (result.rowCount === 0) {
+            return res.status(404).json({
+                message: "Transaction not found"
+            });
+        }
+
+        res.json(result.rows[0]);
+
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "something went wrong"
+        });
+    }
     
 };
+
+const createTransaction = async (req, res) => {
+    
+    try {
+        
+        const { user_id,
+            category_id,
+            recurring_transaction_id,
+            description,
+            amount,
+            transaction_type } = req.body;
+            
+            const normalizedTransactionType = transaction_type?.toLowerCase();
+
+            if (
+                user_id === undefined ||
+                category_id === undefined ||
+                amount === undefined ||
+                !transaction_type
+            ) {
+                return res.status(400).json({
+                    message: "Missing required fields"
+                });
+            }
+            if ( typeof amount !== "number" ) {
+                return res.status(400).json({
+                    message: "Amount must be a number"
+                });
+            }
+            if( transaction_type !== "income" && transaction_type !== "expense" ){
+                return res.status(400).json({
+                    message: "invalid transaction type!"
+                });
+            }
+            const query = `
+            INSERT INTO transactions (
+                user_id,
+                category_id,
+                recurring_transaction_id,
+                description,
+                amount,
+                transaction_type
+                )
+                VALUES ($1, $2, $3, $4, $5, $6)
+                RETURNING *;
+                `;
+        const values = [
+            user_id,
+            category_id,
+            recurring_transaction_id,
+            description,
+            amount,
+            transaction_type
+        ];
+        const result = await pool.query(query, values);
+        console.log(result.rows);
+        res.status(201).json(result.rows[0]);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "something went wrong"
+        });
+    }
+};
+
+
 
 module.exports = {
     getTransactions,
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+const pool = require("./config/db");
+
 app.use(express.json());
 
 const transactionRoutes = require("./routes/transactionRoutes");
